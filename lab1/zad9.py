9c84ca51364cb52c7c635c46253c8740a399b63c
AzurefGit <l.malinowski2003@gmail.com>
2025-10-07 17:14:52 +0200

    Update zad9.py

--- lab1/zad9.py
+++ lab1/zad9.py
@@ -2,20 +2,27 @@ import asyncio
 
 
 async def maszyna_a() -> None:
-    await asyncio.sleep(2)
-    print("A pracuje")
+    licznik = 0
+    while licznik < 15:
+        await asyncio.sleep(2)
+        print("A pracuje")
+        licznik += 2
 
 async def maszyna_b() -> None:
-    await asyncio.sleep(3)
-    print("B pracuje")
+    licznik = 0
+    while licznik < 15:
+        await asyncio.sleep(3)
+        print("B pracuje")
+        licznik += 3
 
 async def maszyna_c() -> None:
-    await asyncio.sleep(5)
-    print("C pracuje")
+    licznik = 0
+    while licznik < 15:
+        await asyncio.sleep(5)
+        print("C pracuje")
+        licznik += 5
 
 async def main() -> None:
-    await asyncio.gather(maszyna_a(), maszyna_b(), maszyna_c())
-    await asyncio.gather(maszyna_a(), maszyna_b(), maszyna_c())
     await asyncio.gather(maszyna_a(), maszyna_b(), maszyna_c())
 
 if __name__ == "__main__":
